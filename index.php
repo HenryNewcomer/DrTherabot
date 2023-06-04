45fd23af91fd8e26c74a8f02710b8522def80060
Henry Newcomer <a.cliche.email@gmail.com>
2023-06-04 18:01:30 -0500

    Add tabs and original CSS.

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+    require_once(__DIR__ . '/chat.php');
+    $_POST['welcome_message'] = !empty($_POST['welcome_message']) ? $_POST['welcome_message'] : "[start]";
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Therabotist :: Your personal GPT-powered therapist. Always available when you need it. ALWAYS FREE &amp; ALWAYS ANONYMOUS.</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script>let fullConvo = [<?php echo "['system', '" . $_POST['welcome_message'] . "']"; ?>];</script>
+    <script src="/js/global.js" async defer></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bebas+Neue|Montserrat|Cormorant+Garamond|Material+Icons">
+</head>
+<body>
+    <header>
+        <div class="loading_indicator"></div>
+        <div class="errors"></div>
+    </header>
+    <main class="message_window">
+        <div id="messages"></div>
+        <div class="your_message">
+            <label class="header" for="prompt">Your message:</label>
+            <div class="container">
+                <form class="sender_input" id="sender_input">
+                    <input type="text" id="newPrompt" name="newPrompt" placeholder="Go ahead :) Say 'hi'">
+                </form>
+                <div class="sender_button_container">
+                    <button class="send_message" id="submit"><i class="material-icons">send</i></button>
+                </div>
+            </div>
+        </div>
+        <?php include_once(__DIR__ . '/tabs.php'); ?>
+    </main>
+    <footer>
+        <p>Copyright &copy; Henry Newcomer 2023</p>
+    </footer>
+</body>
+</html>
---
+++ tabs.php
@@ -0,0 +1,32 @@
+<div id="tabs">
+    <nav class="tabbed-folder">
+    <ul>
+        <li><a href="#what-is-this" class="tab">What is this?</a></li>
+        <li><a href="#anonymous" class="tab">Always anonymous</a></li>
+        <li><a href="#about-me" class="tab">About the Developer</a></li>
+    </ul>
+    </nav>
+    <div class="tabbed-content" id="what-is-this">
+        <h2>What is this?</h2>
+        <p>Text</p>
+        <ul>
+            <li>List item A</li>
+            <li>List item B</li>
+            <li>List item C</li>
+        </ul>
+        <p>Here are some additional tips for interacting with LuvBot:</p>
+        <ul>
+            <li>Be clear and concise in your messages. Therabotist currently uses OpenAI's ChatGPT 3.5, which is an LLM (Large Language Model). At this point in time, all LLMs are still under development. They may not always understand your messages if they are not clear and concise, <strong>and may provide inaccurate information.</strong></li>
+            <li>Use natural language. Therabotist's LLM is trained on a massive dataset of text and code. It can understand natural language, so you can talk to it like you would talk to a friend.</li>
+            <li>Be patient. Therabotist is still under development, so it may not always be able to answer your questions or complete your requests immediately. This is especially true during times of high-traffic that OpenAI may be experiencing. Check OpenAI's <a href="https://status.openai.com">Status</a> page for up-to-date info about their servers.</li>
+        </ul>
+    </div>
+    <div class="tabbed-content" id="anonymous">
+        <h2>Always anonymous</h2>
+        <p>TEXT</p>
+    </div>
+    <div class="tabbed-content" id="about-me">
+        <h2>Help</h2>
+        <p>TEXT</p>
+    </div>
+</div>
