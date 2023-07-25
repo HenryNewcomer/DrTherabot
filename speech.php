8b9eb120bf72286ace5f7dea462348d51c226ca0
Henry Newcomer <a.cliche.email@gmail.com>
2023-07-24 21:57:46 -0500

    Add initial speech files

---
+++ speech.php
@@ -0,0 +1,43 @@
+<?php
+
+// NOTE: When this page is called, if there's a POST request, the output is a string.
+//       So it's actions should be different as we're not rendering the entire page.
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use GuzzleHttp\Psr7\Utils;
+
+if (!isset($_FILES['audio']) || empty($_FILES['audio'])) {
+    die('Invalid request.');
+}
+
+$client = OpenAI::client('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'); 
+$audio_format = 'audio/mpeg-3';
+
+if (($_FILES['audio']['name'] != 'blob') || $_FILES['audio']['full_path'] != 'blob') {
+    die('Naming error.');
+}
+
+$files_filtered = array(
+    'tmp_name' => htmlspecialchars($_FILES['audio']['tmp_name']),
+    'size' => filter_var($_FILES['audio']['size'], FILTER_SANITIZE_NUMBER_INT)
+);
+
+// Save the uploaded file to a temporary location with .mp3 extension
+$temp_filename = tempnam(sys_get_temp_dir(), 'openai') . '.mp3';
+move_uploaded_file($files_filtered['tmp_name'], $temp_filename);
+
+$stream = Utils::streamFor(fopen($temp_filename, 'r'));
+
+$response_text = $client->audio()->transcribe([
+    "model" => "whisper-1",
+    "file" => $stream,
+    "response_format" => 'verbose_json'
+]);
+
+// Remember to delete the temporary file
+unlink($temp_filename);
+
+var_dump($response_text);
+
+?>
